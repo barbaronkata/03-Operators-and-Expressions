851ccf766c3e0bb0225b2b530260669d133f47ce
Galina <galinasariiska@gmail.com>
2015-01-27 19:42:17 +0200

    HW
    HW

---
+++ 04-Rectangles/04-Rectangles.cs
@@ -0,0 +1,22 @@
+﻿//Problem 4. Rectangles
+
+//Write an expression that calculates rectangle’s perimeter and area by given width and height.
+
+using System;
+using System.Globalization;
+using System.Threading;
+
+class RectangleArea
+{
+    static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Console.Write("Please enter width: ");
+        double rectangleWidth = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Please enter height: ");
+        double rectangleHeight = Convert.ToDouble(Console.ReadLine());
+        double rectangleArea = rectangleWidth * rectangleHeight;
+        double rectanglePerimeter = 2 * (rectangleWidth + rectangleHeight);
+        Console.WriteLine("Width: {0}\nHeight: {1}\nPerimeter: {2}\nArea: {3}", rectangleWidth, rectangleHeight, rectanglePerimeter, rectangleArea);
+    }
+}
---
+++ 14-Modify a Bit at Given Position/14-Modify a Bit at Given Position.cs
@@ -0,0 +1,32 @@
+﻿//Problem 14. Modify a Bit at Given Position
+//We are given an integer number n, a bit value v (v=0 or 1) and a position p.
+//Write a sequence of operators (a few lines of C# code) that modifies n to hold the
+// value v at the position p from the binary representation of n while preserving all other bits in n.
+
+using System;
+
+class Modify
+{
+    static void Main(string[] args)
+    {
+        Console.Write("n:");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("p:");
+        int p = int.Parse(Console.ReadLine());
+        Console.Write("v:");
+        int v = int.Parse(Console.ReadLine());
+        int c = 1;
+        if (v == 1)
+        {
+            int mask = (c << p);
+            int result = mask | n;
+            Console.WriteLine("result: " + result);
+        }
+        if (v == 0)
+        {
+            int mask1 = ~(c << p);
+            int result = mask1 & n;
+            Console.WriteLine("result: " + result);
+        }
+    }
+}
